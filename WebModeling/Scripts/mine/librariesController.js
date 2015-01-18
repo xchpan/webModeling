9a4925cbc0902dae422f293aa811b26dc84d7e43
xchpan <xchpan@gmail.com>
2015-01-18 02:16:34 -0800

    refresh the library accordion

--- WebModeling/Scripts/mine/librariesController.js
+++ WebModeling/Scripts/mine/librariesController.js
@@ -1,15 +1,20 @@
 ﻿libraries.controller("librariesController", [
-        '$scope', 'librariesService', function($scope, librariesService) {
+        '$scope', '$timeout', 'librariesService', function($scope, $timeout, librariesService) {
             librariesService.getAllLibraries().success(function(data) {
                 $scope.libraries = data;
-                //$("#libraryList").accordion();
+                $timeout(function() {
+                    $("#libraryList").accordion();
+                });
             });
 
             $scope.deleteLibrary = function(libraryId) {
                 librariesService.deleteLibrary(libraryId).success(function() {
                     var index = findIndex($scope.libraries, libraryId);
                     $scope.libraries.splice(index, 1);
-                }).error(function(data, status, headers, config) {
+                    $timeout(function () {
+                        $("#libraryList").accordion('refresh');
+                    });
+                }).error(function (data, status, headers, config) {
                     alert("error: " + data);
                 });
             };
@@ -17,20 +22,10 @@
             $scope.createLibrary = function() {
                 librariesService.createLibrary().success(function(data) {
                     $scope.libraries.push(data);
+                    $timeout(function () {
+                        $("#libraryList").accordion('refresh');
+                    });
                 });
             };
         }
-    ])
-    .directive('libraryAccordion', function($timeout) {
-        return {
-            scope: {
-                myData: '=libraryAccordion'
-            },
-            template: '<h3 ng-repeat-start="library in myData">{{library.Name}}</h3><div ng-repeat-end class="librayContainer"><span ng-repeat="item in library.Items" class="libraryItem"><img src="{{item.Icon}}" class="modelTypeIcon"/><div>{{item.Name}}</div></span></div>',
-            link: function(scope, element) {
-                $timeout(function() {
-                    $(element).accordion();
-                }, 0);
-            }
-        }
-    });
+    ]);
