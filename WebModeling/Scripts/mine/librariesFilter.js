09b60d7d726a035f42b7d87ffb41e5aba366a007
xchpan <xchpan@gmail.com>
2015-02-23 16:42:45 -0800

    Show parameters for submodels

--- WebModeling/Scripts/mine/librariesFilter.js
+++ WebModeling/Scripts/mine/librariesFilter.js
@@ -39,7 +39,31 @@
 
         return parameters;
     };
-}).filter('getSourceParameters', function() {
+}).filter('getSourceParameters', function () {
+    var fillChild = function(type, name, typeName, libraries) {
+        var instance = { Name: name, Parameters: [] };
+        var templateName = typeName.split("/");
+        var library = null;
+        for (var i = 0; i < libraries.length; i++) {
+            if (libraries[i].Name == templateName[0]) {
+                library = libraries[i];
+                break;
+            }
+        };
+        var childTemplate = null;
+        for (i = 0; i < library.Items.length; i++) {
+            if (library.Items[i].Type == type && library.Items[i].Name == templateName[1]) {
+                childTemplate = library.Items[i];
+                break;
+            }
+        }
+        childTemplate.Parameters.forEach(function (p) {
+            instance.Parameters.push(p.Name);
+        });
+
+        return instance;
+    }
+
     return function(model, libraries) {
         var parameters = {TopLevel: [], ChildLevel: []};
         model.Parameters.forEach(function (parameter) {
@@ -47,27 +71,13 @@
         });
         model.Ports.forEach(function(port) {
             if (port.Direction == "In") {
-                var portInstance = { Name: port.Name, Parameters: [] };
-                var templateName = port.PortTemplateName.split("/");
-                var library = null;
-                for (var i = 0; i < libraries.length; i++) {
-                    if (libraries[i].Name == templateName[0]) {
-                        library = libraries[i];
-                        break;
-                    }
-                };
-                var portTemplate = null;
-                for (i = 0; i < library.Items.length; i++) {
-                    if (library.Items[i].Type == "Port" && library.Items[i].Name == templateName[1]) {
-                        portTemplate = library.Items[i];
-                        break;
-                    }
-                }
-                portTemplate.Parameters.forEach(function(p) {
-                    portInstance.Parameters.push(p.Name);
-                });
+                var portInstance = fillChild("Port", port.Name, port.PortTemplateName, libraries);
                 parameters.ChildLevel.push(portInstance);
             }
+        });
+        model.Submodels.forEach(function (child) {
+            var portInstance = fillChild("Model", child.Name, child.ModelTypeName, libraries);
+                parameters.ChildLevel.push(portInstance);
         });
 
         return parameters;
