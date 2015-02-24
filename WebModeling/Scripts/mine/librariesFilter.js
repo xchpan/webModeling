82da3e17a672d80725fff4d4b6212cfefbc67b12
xchpan <xchpan@gmail.com>
2015-02-23 16:48:57 -0800

    Show parameters for parameter connection sink

--- WebModeling/Scripts/mine/librariesFilter.js
+++ WebModeling/Scripts/mine/librariesFilter.js
@@ -31,10 +31,44 @@
         return result;
     };
 }).filter('getSinkParameters', function() {
-    return function(model, libraries) {
-        var parameters = [];
-        model.Parameters.forEach(function(parameter) {
-            parameters.push(parameter.Name);
+    var fillChild = function(type, name, typeName, libraries) {
+        var instance = { Name: name, Parameters: [] };
+        var templateName = typeName.split("/");
+        var library = null;
+        for (var i = 0; i < libraries.length; i++) {
+            if (libraries[i].Name == templateName[0]) {
+                library = libraries[i];
+                break;
+            }
+        };
+        var childTemplate = null;
+        for (i = 0; i < library.Items.length; i++) {
+            if (library.Items[i].Type == type && library.Items[i].Name == templateName[1]) {
+                childTemplate = library.Items[i];
+                break;
+            }
+        }
+        childTemplate.Parameters.forEach(function (p) {
+            instance.Parameters.push(p.Name);
+        });
+
+        return instance;
+    }
+
+    return function (model, libraries) {
+        var parameters = { TopLevel: [], ChildLevel: [] };
+        model.Parameters.forEach(function (parameter) {
+            parameters.TopLevel.push(parameter.Name);
+        });
+        model.Ports.forEach(function (port) {
+            if (port.Direction == "Out") {
+                var portInstance = fillChild("Port", port.Name, port.PortTemplateName, libraries);
+                parameters.ChildLevel.push(portInstance);
+            }
+        });
+        model.Submodels.forEach(function (child) {
+            var portInstance = fillChild("Model", child.Name, child.ModelTypeName, libraries);
+            parameters.ChildLevel.push(portInstance);
         });
 
         return parameters;
